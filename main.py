6a835edcb0f499ff29d5cb825f215ddc3613596d
Miguel Cassimiro <miguel.cassimiro99@gmail.com>
2024-04-21 12:07:13 -0300

    fix: new bot with simple hello world

--- main.py
+++ main.py
@@ -1,30 +1,36 @@
+import os
+from dotenv import load_dotenv
 import discord
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+from discord.ext import commands
 
-client = discord.Client();
+load_dotenv()
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
-scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',
-         "https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
-client_gs = gspread.authorize(creds)
-sheet = client_gs.open('Financial Inflows').sheet1
+# Configura os intents
+intents = discord.Intents.all()
 
-@client.event
+# Cria o bot com os intents configurados
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print("Bot is ready!")
+    guild_count = 0  # Inicializa o contador de servidores
+
+    for guild in bot.guilds:
+        print(f"- {guild.id} (name: {guild.name})")
+        guild_count += 1  # Incrementa o contador para cada guilda
+
+    print("SampleDiscordBot is in " + str(guild_count) + " guilds.")
+
+@bot.event
 async def on_message(message):
-    if message.channel.name == 'new-billings' and message.content.startswith('Descrição:'):
-        try:
-            lines = message.content.split('\n')
-            description = lines[0].split(':')[1].strip()
-            date = lines[1].split(':')[1].strip()
-            value = lines[2].split(':')[1].strip()
-            responsor = lines[3].split(':')[1].strip()
+    if message.content == 'hello':
+        print(message.chaneel.name)
+        await message.channel.send("hello world")
+    
+    # Isso é necessário para processar comandos se você sobrescrever o evento on_message
+    await bot.process_commands(message)
 
-            # Inserir na planilha
-            next_row = len(sheet.get_all_values()) + 1
-            sheet.insert_row([description, date, value, responsor, 'FALSE'], next_row)
-            await message.channel.send("Conta adicionada com sucesso!")
-        except Exception as e:
-            await message.channel.send(f"Erro ao adicionar conta: {str(e)}")
 
-client.run('your_discord_bot_token')
+bot.run(DISCORD_TOKEN)
